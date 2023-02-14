eaa7d96f56ee92a4c775bc8de828a235d50d6c77
SELBY <Gemma.SELBY@education.gov.uk>
2023-02-14 14:25:57 +0000

    Initial code for small reactive chart showing Autumn term absence headlines. Data sourced from Week 4 2023 publication weekly data.

--- global.R
+++ global.R
@@ -4,6 +4,9 @@ shhh(library(shiny))
 shhh(library(ggplot2))
 shhh(library(dplyr))
 shhh(library(styler))
+shhh(library(data.table))
+shhh(library(stringr))
+shhh(library(plotly))
 
 tidy_code_function <- function() {
   message("----------------------------------------")
@@ -20,40 +23,24 @@ tidy_code_function <- function() {
   return(script_changes)
 }
 
+attendance_data <- read.csv(file = "ees_weekly_data_.csv") %>%
+  as.data.frame() %>%
+  mutate(time_identifier = str_remove_all(time_identifier, "Week ")) %>% 
+  mutate_at(c(1:2, 12:63), as.numeric) %>%
+  filter(time_period == '2022')
 
-data <- data.frame(
-  time_period = c(
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021
-  ),
-  area = c(
-    "England", "England", "England", "England", "England", "England", "England", "England",
-    "North East", "North East", "North East", "North East", "North East", "North East", "North East", "North East",
-    "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber",
-    "England", "England", "England", "England", "England", "England", "England", "England",
-    "North East", "North East", "North East", "North East", "North East", "North East", "North East", "North East",
-    "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber",
-    "England", "England", "England", "England", "England", "England", "England", "England",
-    "North East", "North East", "North East", "North East", "North East", "North East", "North East", "North East",
-    "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber", "Yorkshire and the Humber"
-  ),
-  phase = c(
-    "All", "All", "All", "All", "All", "All", "All", "All",
-    "All", "All", "All", "All", "All", "All", "All", "All",
-    "All", "All", "All", "All", "All", "All", "All", "All",
-    "Primary", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary",
-    "Primary", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary",
-    "Primary", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary", "Primary",
-    "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary",
-    "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary",
-    "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary", "Secondary"
-  ),
-  values = c(12. + sample(1:250, 24) / 250, 8. + sample(1:250, 24) / 250, 14. + sample(1:250, 24) / 250)
-)
+# Add geog lookup
+geog_lookup <- attendance_data %>%
+  dplyr::select(geographic_level, region_name, la_name) %>%
+  unique() %>%
+  arrange(region_name, la_name)
+
+geog_levels <- geog_lookup %>%
+  dplyr::select(geographic_level) %>%
+  unique() %>%
+  as.data.table()
+
+school_type_lookup <- attendance_data %>%
+  dplyr::select(geographic_level, school_type) %>%
+  unique() %>%
+  arrange(geographic_level, school_type)
--- ui.R
+++ ui.R
@@ -3,26 +3,42 @@ ui <- function(input, output, session) {
       fluidRow(
         width = 12,
         column(
-          width=6,
-          selectizeInput("filter1",
-            label = "Filter 1",
-            choices = c("England", "North East", "Yorkshire and the Humber"),
-            selected = "England"
+          width=3,
+          selectInput(inputId = "geography_choice",
+                         label = "Choose geographic level:",
+                         choices = c("National","Regional","Local authority"),
+                         selected = head(geog_levels,1)
           )
         ),
         column(
-          width=6,
-          selectizeInput("filter2",
-            label = "Filter 2",
-            choices = c("All", "Primary", "Secondary"),
-            selected = "All"
+          width=3,
+          selectInput(inputId = "region_choice",
+                         label = "Choose region:",
+                         choices = geog_lookup %>% dplyr::filter(geographic_level == 'National') %>% dplyr::select(region_name) %>% unique() %>% as.data.table(),
+                         #selected = head(reg_geog,1)
+          )
+        ),
+        column(
+          width=3,
+          selectInput(inputId = "la_choice",
+                         label = "Choose local authority:",
+                         choices = geog_lookup %>% dplyr::filter(region_name == "East Midlands") %>% dplyr::select(la_name) %>% unique() %>% as.data.table(),
+                         #selected = la_geog()[2,1]
+          )
+        ),
+        column(
+          width=3,
+          selectInput(inputId = "school_choice",
+                         label = "Choose school type:",
+                         choices = school_type_lookup %>% dplyr::filter(geographic_level == "National") %>% dplyr::select(school_type) %>% unique() %>% as.data.table(),
+                         selected = "Primary"
           )
         )
       ),
       fluidRow(
         column(
           12,
-          plotOutput("chart")
+          plotlyOutput("absence_rates_timeseries_plot")
         )
       )
   )
